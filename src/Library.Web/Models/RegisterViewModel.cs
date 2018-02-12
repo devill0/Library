7137d95100bc88276e08c52f66b0c18ccc1085ba
devill0 <marcinn1994@gmail.com>
2018-02-12 20:46:07 +0100

    Login&Register view models

---
+++ src/Library.Web/Models/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Library.Core.Dto;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Web.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        public string PasswordConfirmation { get; set; }
+
+        [Required]
+        public RoleDto Role { get; set; }
+    }
+}
