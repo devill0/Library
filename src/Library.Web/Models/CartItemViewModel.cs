5f5e8fbae4d60e9b0f499c0943bc5b17464abee8
devill0 <marcinn1994@gmail.com>
2018-02-13 22:51:06 +0100

    Cart&CartItem Models

---
+++ src/Library.Web/Models/CartItemViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Library.Web.Models
+{
+    public class CartItemViewModel
+    {
+        public Guid BookId { get; set; }
+        public string Title { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get { return Quantity * UnitPrice; } }
+    }
+}
