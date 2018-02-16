6375038e7ab204872484bae87c58aab7ebc6dea4
devill0 <marcinn1994@gmail.com>
2018-02-16 11:07:42 +0100

    CartItem and Cart outline

---
+++ src/Library.Core/Domain/Cart.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Library.Core.Domain
+{
+    public class Cart
+    {
+        private readonly ISet<CartItem> items = new HashSet<CartItem>();
+        public IEnumerable<CartItem> Items => items;
+        public decimal TotalPrice { get; set; }
+
+        public void DeleteBook(Guid id)
+        {
+            var book = items.SingleOrDefault(b => b.BookId == id);
+            if (book == null)
+            {
+                throw new Exception("This element does not exist.");
+            }
+            if (book.Quantity == 1)
+            {
+                items.Remove(book);
+
+                return;
+            }
+            book.DecreaseQuantity();
+        }
+    }
+}
---
+++ src/Library.Core/Domain/CartItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Core.Domain
+{
+    public class CartItem
+    {
+        public Guid BookId { get; }
+        public string Title { get; private set; }
+        public string Author { get; private set; }
+        public int Quantity { get; private set; }
+        public decimal UnitPrice { get; private set; }
+        public decimal TotalPrice => Quantity * UnitPrice;
+
+        public CartItem(Book book)
+        {
+            BookId = book.Id;
+            Title = book.Title;
+            Author = book.Author;
+            UnitPrice = book.Price;
+            Quantity = 1;
+        }
+
+        public void IncreaseQuantity()
+        {
+            Quantity++;
+        }
+
+        public void DecreaseQuantity()
+        {
+            if (Quantity <= 1)
+            {
+                throw new Exception("Quantity can not be lower than 1.");
+            }
+            Quantity--; 
+        }
+    }
+}
