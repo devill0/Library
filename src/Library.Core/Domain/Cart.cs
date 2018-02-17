a89345c40cd256037ff92d2b3e9d0f06c62f2273
devill0 <marcinn1994@gmail.com>
2018-02-17 10:54:17 +0100

    Cart extends

--- src/Library.Core/Domain/Cart.cs
+++ src/Library.Core/Domain/Cart.cs
@@ -11,6 +11,22 @@ namespace Library.Core.Domain
         public IEnumerable<CartItem> Items => items;
         public decimal TotalPrice { get; set; }
 
+        public void AddBook(Book book)
+        {
+            var item = items.SingleOrDefault(i => i.BookId == book.Id);
+            if (item == null)
+            {
+                item = new CartItem(book);
+                items.Add(item);
+
+                return;
+            }
+            item.IncreaseQuantity();            
+        }
+
+        public void ClearCart()
+            => items.Clear();
+
         public void DeleteBook(Guid id)
         {
             var book = items.SingleOrDefault(b => b.BookId == id);
